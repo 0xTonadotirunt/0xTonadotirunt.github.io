295c37e00339ba581137f871dcbe8c98fce86927
Zhan Rong <tonadotirunt@gmail.com>
2022-10-14 18:02:13 +0800

    finished home page

--- resources/index.js
+++ resources/index.js
@@ -3,3 +3,19 @@ $(document).ready(function () {
     $("#sidebar").toggleClass("active");
   });
 });
+
+const app = Vue.createApp({
+  // Data Properties
+  data() {
+    return {};
+  },
+
+  methods: {
+    mouseOver: function () {
+      alert("Mouse Over!");
+    },
+    // YOUR CODE GOES HERE
+  },
+});
+
+app.mount("#app");
